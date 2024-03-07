2f4bbf0de7c88effcaa6de2d75eb2e376914462e
Chapelk1 <klepach154789@gmail.com>
2024-03-07 17:10:12 +0200

    finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
+  spaceBetween: 300,
 
   pagination: {
     el: ".swiper-pagination",
@@ -21,5 +22,39 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  effect: "coverflow",
+  speed: 1000,
+});
+
+const swiperSpecialized = new Swiper(".swiper-specialized-solutions", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 2,
+  // autoplay: {
+  //   delay: 3000,
+  // },
+  // speed: 1000,
+  breakpoints: {
+    570: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1180: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    1670: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
   },
 });
